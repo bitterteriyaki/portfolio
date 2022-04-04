dcae94aaca00ff655ebc935453b11fc09a157c59
kyomi <contato.kyomi@gmail.com>
2022-04-04 17:31:37 -0300

    Add header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app'
 
 import ScrollIndicator from '../components/app/ScrollIndicator'
+import Header from '../components/app/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ScrollIndicator />
-      <Component {...pageProps} />
+      <div className='container'>
+        <Header />
+        <Component {...pageProps} />
+      </div>
     </>
   )
 }
