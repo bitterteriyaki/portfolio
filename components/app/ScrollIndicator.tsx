c41c6e6c862c71e9b8ff85f3a3f90b38a200e617
kyomi <contato.kyomi@gmail.com>
2022-04-03 21:08:34 -0300

    Add scroll indicator

---
+++ components/app/ScrollIndicator.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+
+import styles from '../../styles/app/ScrollIndicator.module.css'
+
+const ScrollIndicator: NextPage = () => {
+  const [offset, setOffset] = useState<number>()
+  const style = { width: `${offset}%` }
+
+  const handleScroll = () => {
+    const maxHeight = document.body.scrollHeight - window.innerHeight
+    const percentage = (window.scrollY / maxHeight) * 100
+    setOffset(percentage)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
+  return <div className={styles.indicator} style={style}></div>
+}
+
+export default ScrollIndicator
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import ScrollIndicator from '../components/app/ScrollIndicator'
+import '../styles/globals.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ScrollIndicator />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
