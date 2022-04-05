ade0989c683c028abafbbd407ed92c5bcbb085ab
kyomi <contato.kyomi@gmail.com>
2022-04-04 21:21:30 -0300

    Escape apostrophe and add `rel` parameter to `<a>` tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,18 @@ import styles from '../styles/pages/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <main>q
+    <main>
       <section className={styles.intro}>
         <div className={styles.name}>
           <h2>Hi!</h2>
           <h1>My name is kyomi.</h1>
           <h3>
-            I'm a junior full-stack developer who enjoys building
+            I&apos;m a junior full-stack developer who enjoys building
             exceptional and scalable applications. Currently studying
             Software Engineering at one of the{` `}
-            <a href='https://international.unb.br' target='_blank'>best colleges in Brazil</a>.
+            <a href='https://international.unb.br' target='_blank' rel='noreferrer'>
+              best colleges in Brazil
+            </a>.
           </h3>
         </div>
         <div className={styles.github}>
