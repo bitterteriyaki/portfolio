6a2992148a65cd476237d82c4faaefa189f7e80f
kyomi <personal@kyomi.codes>
2023-03-25 18:50:29 -0300

    feat: add dynamic import to FAQ component

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,22 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-import FAQ from '@/components/groups/FAQ';
 import Hero from '@/components/groups/Hero';
+
+// Since the FAQ component can take a while to load, we use dynamic import
+// to load it only when the user is on the home page.
+const DynamicFAQ = dynamic(() => import('@/components/groups/FAQ'), {
+  loading: () => (
+    <div className="mb-5 grid place-items-center gap-5">
+      <div className="h-56 w-full max-w-lg animate-pulse rounded-lg bg-zinc-800" />
+      <div className="h-56 w-full max-w-lg animate-pulse rounded-lg bg-zinc-800" />
+    </div>
+  ),
+  ssr: false,
+});
 
 function Home() {
   return (
@@ -36,7 +48,7 @@ function Home() {
       <main className="flex justify-center">
         <div className="grid w-11/12 max-w-7xl gap-1 md:w-5/6">
           <Hero />
-          <FAQ />
+          <DynamicFAQ />
         </div>
       </main>
     </>
