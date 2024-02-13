70ffa9af07edd549fad673798e2ec3954bf642f7
kyomi <me@kyomi.dev>
2024-02-13 09:42:48 -0300

    feat: add many modules to `nuxt`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/sitemap',
+    '@nuxt/image',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/device',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/robots',
+    'nuxt-icon',
+    '@nuxt/content',
   ],
   eslint: {
     lintOnStart: false,
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: { 'DM+Sans': [400, 500, 600, 700] },
   },
 })
