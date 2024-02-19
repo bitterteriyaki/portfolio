45650246339b26b11e4ccdfe250e4225b3528ea5
kyomi <me@kyomi.dev>
2024-02-19 20:52:54 -0300

    test(app): test the `app` component

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitest/config'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
 
-export default defineConfig({
+export default defineVitestConfig({
   test: {
     coverage: {
       reporter: ['text', 'cobertura'],
